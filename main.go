844f82d80c14337901d60fa3c7fa72b2119e8a8c
EMON <jakariachowdhury2427@gmail.com>
2026-04-30 22:31:46 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	name := "Emon"
+	name2 := "Sumu"
+	age := 21
+	age2 := 19
+
+	fmt.Println(name)
+	fmt.Println(name2)
+	fmt.Println(age)
+	fmt.Println(age2)
+}
